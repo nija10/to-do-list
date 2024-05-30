dc45d75163c7551adf502af80750aa2ef207dcb2
nija10 <nijakharangar@gmail.com>
2024-05-30 21:38:20 +0530

    functionality to add items in to-do list

---
+++ src/script.js
@@ -0,0 +1,16 @@
+let inputBox = document.getElementById("input-box");
+let listContainer = document.getElementById("list-container");
+
+function addTask() {
+  if (inputBox.value === "") {
+    alert("Write something!");
+  } else {
+    let li = document.createElement("li");
+    li.innerHTML = inputBox.value;
+    listContainer.appendChild(li);
+    let span = document.createElement("span");
+    span.innerHTML = "\u00d7";
+    li.appendChild(span);
+  }
+  inputBox.value = ""; //clear input field after adding item
+}
