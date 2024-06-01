87050292254a11ce56f543edd53d9490f3fa770d
nija10 <nijakharangar@gmail.com>
2024-06-01 22:11:01 +0530

    function to save and display data when page loaded

--- src/script.js
+++ src/script.js
@@ -13,12 +13,27 @@ function addTask() {
     li.appendChild(span);
   }
   inputBox.value = ""; //clear input field after adding item
+  saveData();
 }
 
 listContainer.addEventListener("click", function (e) {
   if (e.target.tagName === "LI") {
     e.target.classList.toggle("checked");
+    saveData();
   } else if (e.target.tagName === "SPAN") {
     e.target.parentElement.remove();
+    saveData();
   }
 });
+
+//save list items in "data"
+function saveData() {
+  localStorage.setItem("data", listContainer.innerHTML);
+}
+
+//display data each time page loaded
+function showTask() {
+  listContainer.innerHTML = localStorage.getItem("data");
+}
+
+showTask();
